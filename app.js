1eb52efa4f84351e8c04a218845e5c5d7a128fca
egor <kerminegor@gmail.com>
2021-03-26 14:21:09 +0300

    bug fixed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const out = document.querySelector('output'),
 panel = document.querySelector('.panel')
-const elemInCulc = [7,8,9,4,5,6,1,2,3,'*',0,'=','+','-','/','C']
+const elemInCulc = [7,8,9,4,5,6,1,2,3,'*',0,'=','+','-','/','C','.']
 
 elemInCulc.map(symbol=>{
     let btn = document.createElement('button')
@@ -20,16 +20,20 @@ btn.forEach(btn=>{
 
 function mathCucl(value){
   
-    if(value === '=') out.textContent = eval(out.textContent)
+    if(value === '=') {
+        out.textContent = out.textContent.replace(",",".")
+        out.textContent = eval(out.textContent)
+        out.textContent = out.textContent.replace(".",",") 
+    }
     
     if(value!='=') out.textContent+=value
-    if((value == '/' || value == '+' || value == '-' || value == '*')&&(out.textContent.slice(0,1)=='/' || out.textContent.slice(0,1)=='+' || out.textContent.slice(0,1)=='-'|| out.textContent.slice(0,1)=='*')){
+    if((value == '/' || value == '+' || value == '-' || value == '*'||value == '.')&&(out.textContent.slice(0,1)=='/' || out.textContent.slice(0,1)=='+' || out.textContent.slice(0,1)=='-'|| out.textContent.slice(0,1)=='*'||out.textContent.slice(0,1)=='.')){
         out.textContent = 'выражение неверно'
         setTimeout(()=>{
             out.textContent = ''
         },1000)
     } 
-    if((value =='/' ||value == '+' || value =='-'|| value =='*') && (out.textContent.slice(-2,-1)=='/'||out.textContent.slice(-2,-1)=='+'|| out.textContent.slice(-2,-1)=='-'||out.textContent.slice(-2,-1)=='*')){
+    if((value =='/' ||value == '+' || value =='-'|| value =='*'||value =='.') && (out.textContent.slice(-2,-1)=='/'||out.textContent.slice(-2,-1)=='+'|| out.textContent.slice(-2,-1)=='-'||out.textContent.slice(-2,-1)=='*'||out.textContent.slice(-2,-1)=='.')){
         let str= out.textContent
        str = str.split('')
         let b = str.pop()
@@ -46,7 +50,7 @@ function mathCucl(value){
     if(out.textContent.length>29){
         let oldValue = out.textContent
         out.textContent = 'переполнено'
-        btn.removeEventListener('click',mathCuclEv)
+        // btn.removeEventListener('click',mathCuclEv)
         
         setTimeout(()=>{
            out.textContent = oldValue  
